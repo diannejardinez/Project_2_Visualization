c22d430b892d6609a429efd298ce45808b1b280b
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-06-25 00:58:11 -0700

    plotting through event index number only

--- Test-HTML/static/js/app.js
+++ Test-HTML/static/js/app.js
@@ -5,12 +5,15 @@
 
 
 
-//Setting global variable data to athleteData
-var athleteData = athleteData; 
+// //Setting global variable data to athleteData
+// var athleteData = athleteData; 
+
+// Setting variable for optionChanged() metaDataIndex
+var metaDataIndex;
 
 // Populate dropdown options with the participant id names
 d3.json("data/combined_bodycomp_age.json").then((athleteData) => {
-	console.log(athleteData)
+	// console.log(athleteData)
 
 	function filteredTableOptions(htmlId, athleteDataKey) {
 
@@ -19,7 +22,7 @@ d3.json("data/combined_bodycomp_age.json").then((athleteData) => {
 
 	  // Make a new array with only unique values and populate dropdown options with those values
 	  var populatedOptions = Array.from(new Set(athleteData.map(athleteDataKey)));
-	  populatedOptions.forEach(option => filterOption.append('option').attr('value', option).text(option).order());
+	  populatedOptions.forEach(option => filterOption.append('option').attr('value', option).text(option));
 	};
 
 	// Calling function to auto populate dropdown options
@@ -33,28 +36,43 @@ d3.json("data/combined_bodycomp_age.json").then((athleteData) => {
 
 
 
-// // // Populate dropdown options with the participant id names
-// // d3.json("static/js/Export_DataFrame.json").then((importedData) => {
-// // 	for(var i = 0; i < importedData.length; i++) {
-// // 		var newSports = importedData[i].Sport
-// // 			newSports.forEach(option => filterOption.append('option').attr('value', option).text(option));
-// // 	console.log(importedData[i].Sport)
-// // 	console.log(importedData[i].Event)
-// // 	console.log(importedData[i].Year)
+
+// // function optionChanged() {
+//   // Setting index number for creatingMetaData() and createPlot()
+//   function setIndexNum(value) {
+    d3.json("data/combined_bodycomp_age.json").then((importedData) => {
+      for(var i = 0; i < importedData.length; i++) {
+      		// console.log(importedData[i].Sport)
+      		// console.log(importedData[i].Event)
+      		// console.log(importedData[i].Year)
+      		// console.log(importedData[i].Age)
+      		// console.log(importedData[i].Height)
+      		// console.log(importedData[i].Weight)
+      		// console.log([importedData[i].Age,importedData[i].Height, importedData[i].Weight])
+      		// console.log("first i:" + i)
+      		// console.log("second i:" + (i + 1))
+      		// console.log("third i:" + (i + 2))
+//           if(importedData[i].Sport === value) {
+//               console.log("i for setIndexNum() = " + i)
+//               metaDataIndex = i;
+//           }
+      }
+//       // Setting metaDataIndex as indexNumber
+//       // indexNumber = metaDataIndex
+
+//       // Calling creatingMetaData()
+//       // creatingMetaData (indexNumber)
+//       // console.log("creatingMetaData() in optionChanged() indexNumber = " + indexNumber)
+    
+//       // Calling createPlot()
+//       // createPlot (indexNumber)
+//       // console.log("createPlot() in optionChanged() indexNumber = " + indexNumber)
+    });
+//   }
+//   setIndexNum(0)
+//   // // var dropDownoption = filterOption.node().value
+//   // setIndexNum(Number(dropDownoption))
+// // }
 
 
-// // 	}
-// //   // var newNames = importedData.names
-// //   //   // console.log(newNames)
-// //   //   newNames.forEach(option => filterOption.append('option').attr('value', option).text(option));
-// // }); 
 
-
-// // d3.json("static/js/Export_DataFrame.json").then((importedData) => {
-// //   for(var i = 0; i < importedData.length; i++) {
-// //       if((importedData[i].Event)['id'] === value) {
-// //           console.log("i for setIndexNum() = " + i)
-// //           metaDataIndex = i;
-// //       }
-// //   }
-// //  });
--- Test-HTML/static/js/plots.js
+++ Test-HTML/static/js/plots.js
@@ -1,171 +1,184 @@
+     // d3.json("data/combined_bodycomp_age.json").then((importedData) => {
+     //   // console.log(importedData)
+     //  });
 
 
-var xValue = ['Age', 'Height', 'Weight'];
-
-// Women's Basketball
-var yValue2 = [26, 185, 74];
-
-var trace2 = {
-  name: "Gold",
-  x: xValue,
-  y: yValue2,
-  type: 'bar',
-  text: yValue2.map(String),
-  textposition: 'auto',
-  hoverinfo: 'none',
-  opacity: 0.5,
-  marker: {
-    color: 'rgba(218,165,32,.7)',
-    line: {
-      color: 'rgba(218,165,32,1)',
-      width: 1.5
-    }
-  }
-};
 
 
-var data2 = [trace2];
-// var data = [trace1,trace2,trace3];
+function createPlot (indexNumber) { 
+     d3.json("data/combined_bodycomp_age.json").then((importedData) => {
+       console.log(importedData)
+      // // Use the map method with the arrow function to return all out_ids and their top 3 values
+      // var newIds = importedData.samples.map(data=>(data.otu_ids).slice(0,3))
 
-var layout2 = {
-  // title: "Age and Body Composition for Men's Basketball",
-  title: {
-    text: "Basketball Women's Basketball",
-    font: {
-      size: 12,
-    }
-  },
-  autosize: false,
-  width: 250,
-  height: 500,
-  margin: {
-    l: 50,
-    r: 50,
-    b: 100,
-    t: 100,
-    pad: 4
-  },
-  xaxis: {
-    title: {
-      text: '(Metric System)',
-      font: {
-        size: 10,
+      // // Use the map method with the arrow function to return all sample_values and their top 3values
+      // var newLabels = importedData.samples.map(data=>(data.otu_labels).slice(0,3))
+
+      // // Use the map method with the arrow function to return all sample_values and their top 3 values
+      // var newValues = importedData.samples.map(data=>(data.sample_values).slice(0,3))
+
+      // // Getting the washing frequency
+
+
+
+      // Setting Age, Height, and Weight as the xvalue
+      var xValue = ['Age', 'Height', 'Weight'];
+
+
+      // Setting yvalue
+      var yValue1 = [importedData[indexNumber].Age,importedData[indexNumber].Height, importedData[indexNumber].Weight]
+      var yValue2 = [importedData[indexNumber +1].Age,importedData[indexNumber + 1].Height, importedData[indexNumber + 1].Weight]
+      var yValue3 = [importedData[indexNumber + 2].Age,importedData[indexNumber +2].Height, importedData[indexNumber +2].Weight]
+
+      // Setting textvalue 
+      var textValue1 = (importedData[indexNumber].Event + " for "+ "<br>" + importedData[indexNumber].Country + " in " + importedData[indexNumber].Year)
+      var textValue2 = (importedData[indexNumber +1].Event + " for "+ "<br>" + importedData[indexNumber +1].Country + " in " + importedData[indexNumber +1].Year)
+      var textValue3 = (importedData[indexNumber +2].Event + " for "+ "<br>" + importedData[indexNumber +2].Country + " in " + importedData[indexNumber +2].Year)
+
+      //  TO GET TOP 3 FOR EACH SPORT, NEED TO HAVE BAR CHARTS SLICED FROM 0, 1, 2
+    // WORKS- BUT ONLY GETS THE FIRST THREE IN INDEX - WANT TO GET FIRST THREE UNIQUE EVENTS
+      function barChart1() {
+
+
+        var barChart = {
+          name: "Gold",
+          x: xValue,
+          y: yValue1,
+          type: 'bar',
+          text: yValue1.map(String),
+          textposition: 'auto',
+          hoverinfo: 'none',
+          opacity: 0.5,
+          marker: {
+            color: 'rgba(218,165,32,.7)',
+            line: {
+              color: 'rgba(218,165,32,1)',
+              width: 1.5
+            }
+          }
+        };
+
+        var layout = {
+          title: {
+            text: textValue1,
+            font: {
+              size: 12,
+            }},
+          autosize: false,
+          width: 250,
+          height: 500,
+          margin: {
+            l: 50,
+            r: 50,
+            b: 100,
+            t: 100,
+            pad: 4},
+          xaxis: {
+            title: {
+              text: '(Metric System)',
+              font: {
+                size: 10,
+              }}}
+        };
+
+        Plotly.newPlot("plot1", [barChart], layout);
       }
-    }
-  }
-};
+      
+      function barChart2() {
+        var barChart = {
+          name: "Gold",
+          x: xValue,
+          y: yValue2,
+          type: 'bar',
+          text: yValue2.map(String),
+          textposition: 'auto',
+          hoverinfo: 'none',
+          opacity: 0.5,
+          marker: {
+            color: 'rgba(218,165,32,.7)',
+            line: {
+              color: 'rgba(218,165,32,1)',
+              width: 1.5
+            }
+          }
+        };
 
+        var layout = {
+          title: {
+            text: textValue2,
+            font: {
+              size: 12,
+            }},
+          autosize: false,
+          width: 250,
+          height: 500,
+          margin: {
+            l: 50,
+            r: 50,
+            b: 100,
+            t: 100,
+            pad: 4},
+          xaxis: {
+            title: {
+              text: '(Metric System)',
+              font: {
+                size: 10,
+              }}}
+        };
 
-
-// Women's Archery
-var yValue1 = [21, 167, 59];
-
-var trace1 = {
-  name: "Gold",
-  x: xValue,
-  y: yValue1,
-  type: 'bar',
-  text: yValue1.map(String),
-  textposition: 'auto',
-  hoverinfo: 'none',
-  opacity: 0.5,
-  marker: {
-    color: 'rgba(218,165,32,.7)',
-    line: {
-      color: 'rgba(218,165,32,1)',
-      width: 1.5
-    }
-  }
-};
-
-
-var data1 = [trace1];
-// var data = [trace1,trace2,trace3];
-
-var layout1 = {
-  title: {
-    text: "Archery Women's Team",
-    font: {
-      size: 12,
-    }
-  },
-  autosize: false,
-  width: 250,
-  height: 500,
-  margin: {
-    l: 50,
-    r: 50,
-    b: 100,
-    t: 100,
-    pad: 4
-  },
-  xaxis: {
-    title: {
-      text: '(Metric System)',
-      font: {
-        size: 10,
+        Plotly.newPlot("plot2", [barChart], layout);
       }
-    }
-  }
-};
+      
 
+      function barChart3() {
+        var barChart = {
+          name: "Gold",
+          x: xValue,
+          y: yValue3,
+          type: 'bar',
+          text: yValue3.map(String),
+          textposition: 'auto',
+          hoverinfo: 'none',
+          opacity: 0.5,
+          marker: {
+            color: 'rgba(218,165,32,.7)',
+            line: {
+              color: 'rgba(218,165,32,1)',
+              width: 1.5
+            }
+          }
+        };
 
-// Women's Boxing
-var yValue3 = [31, 164, 51];
+        var layout = {
+          title: {
+            text: textValue3,
+            font: {
+              size: 12,
+            }},
+          autosize: false,
+          width: 250,
+          height: 500,
+          margin: {
+            l: 50,
+            r: 50,
+            b: 100,
+            t: 100,
+            pad: 4},
+          xaxis: {
+            title: {
+              text: '(Metric System)',
+              font: {
+                size: 10,
+              }}}
+        };
 
-var trace3 = {
-  name: "Gold",
-  x: xValue,
-  y: yValue3,
-  type: 'bar',
-  text: yValue3.map(String),
-  textposition: 'auto',
-  hoverinfo: 'none',
-  opacity: 0.5,
-  marker: {
-    color: 'rgba(218,165,32,.7)',
-    line: {
-      color: 'rgba(218,165,32,1)',
-      width: 1.5
-    }
-  }
-};
-
-
-var data3 = [trace3];
-// var data = [trace1,trace2,trace3];
-
-var layout3 = {
-  title: {
-    text: "Boxing Women's Flyweight",
-    font: {
-      size: 12,
-    }
-  },
-  autosize: false,
-  width: 250,
-  height: 500,
-  margin: {
-    l: 50,
-    r: 50,
-    b: 100,
-    t: 100,
-    pad: 4
-  },
-  xaxis: {
-    title: {
-      text: '(Metric System)',
-      font: {
-        size: 10,
+        Plotly.newPlot("plot3", [barChart], layout);
       }
-    }
-  }
-};
-
-Plotly.newPlot('plot1', data1, layout1);
-
-Plotly.newPlot('plot2', data2, layout2);
-
-Plotly.newPlot('plot3', data3, layout3);
+      barChart1();
+      barChart2();
+      barChart3();
+    }); 
+}
+createPlot(10) 
 
 
