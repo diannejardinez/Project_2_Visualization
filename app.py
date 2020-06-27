9b1f2470afe12890fd8bff8d739c4f88839f8ad8
Aastha <18218138+Aastha-Arora@users.noreply.github.com>
2020-06-26 20:06:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-import os
-
-import pandas as pd
-# import numpy as np
-
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine
-
-
 from flask import Flask, jsonify, render_template
-# from sqlalchemy import inspect
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import func, desc
 from flask_sqlalchemy import SQLAlchemy
 
 #################################################
@@ -17,111 +8,103 @@ from flask_sqlalchemy import SQLAlchemy
 #################################################
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
 #################################################
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/Olympics"
-#conn = engine.connect()
-#salary_df = pd.read_sql("SELECT * FROM salaries", conn)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/olympics_db"
 db = SQLAlchemy(app)
 db.init_app(app)
-db.metadata.reflect(bind=db.engine, views=True)
 
-# inspector = inspect(db.engine)
-# print(inspector.get_table_names())
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
 
-
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(db.engine, reflect=True)
-
-# print(db.engine.table_names())
-# print(Base.metadata.tables.keys())
+# Save references to the table in database
 # print(Base.classes.keys())
-# Save references to each table
+Athletes = Base.classes.athletes
 
-for t in metadata.sorted_tables:
-    print(t.name)
-
-# Athletes = db.metadata.tables.athletes
-
-
-# print(pd.read_sql_query('select * from athletes', con=db.engine).head())
 
 #################################################
 # Flask Routes
 #################################################
 
 @app.route("/")
-def index():
+def home():
     """Return the homepage."""
     return render_template("index.html")
 
 
-# @app.route('/')
-# def home():
-#     return "Hello World!"
-
-
 # Query the database and send the jsonified results
+@app.route('/api/total-medals')
+def total_medals():
+    """ 
+    Return the the total number of medals won by all the countrie in Summer Olympics held after 1980 
+    """
+    subquery = db.session.query(Athletes.year, Athletes.country, Athletes.event, Athletes.medal)\
+        .filter(Athletes.season == 'Summer')\
+        .filter(Athletes.year >= 1980)\
+        .filter(Athletes.medal.isnot(None))\
+        .distinct()\
+        .subquery()
 
-@app.route('/api/all_athletes')
-def entire_data_0():
-    """Return the complete database for all athletes"""
-    sel = [
-            Athletes.id,
-            Athletes.name,
-            Athletes.sex,
-            Athletes.age,
-            Athletes.height,
-            Athletes.weight, 
-            Athletes.country,
-            Athletes.noc,
-            Athletes.games,
-            Athletes.year,
-            Athletes.season,
-            Athletes.city,
-            Athletes.sport,
-            Athletes.event,
-            Athletes.medal
-            ]
+    query_result = db.session.query(subquery.c.year, subquery.c.country, \
+              func.count(subquery.c.medal).label('total_medals'))\
+        .group_by(subquery.c.year, subquery.c.country)\
+        .order_by(subquery.c.year, desc('total_medals'))\
+        .all()
+    
+    all_country_medals = []
 
-    results = db.session.query(*sel).all()
+    for year, country, totalmedals in query_result:
+        country_medals = {}
+        country_medals["year"] = year
+        country_medals["country"] = country
+        country_medals["total medals"] = totalmedals
+        all_country_medals.append(country_medals)
 
-    all_athletes = []
-
-    for id, name, sex, age, height, weight, country, noc, games,\
-    year, season, city, sport, event, medal in results:
-        altlete_dict = {}
-        altlete_dict["name"] = name
-        altlete_dict["sex"] = sex
-        altlete_dict["age"] = age
-        altlete_dict["height"] = height
-        altlete_dict["weight"] = weight
-        altlete_dict["country"] = country
-        altlete_dict["noc"] = noc
-        altlete_dict["games"] = games
-        altlete_dict["year"] = year
-        altlete_dict["season"] = season
-        altlete_dict["city"] = city
-        altlete_dict["sport"] = sport
-        altlete_dict["event"] = event
-        altlete_dict["medal"] = medal
-        
-        
-        all_athletes.append(altlete_dict)
-
-    # Return a list of the column names (sample names)
-    return jsonify(all_athletes)
+    return jsonify(all_country_medals)
 
 
-@app.route('/api/test1')
-def test_1():
-    return "Work in progress Route test1"
+
+@app.route('/api/event/body-composition/<gender>')
+def gender_body_comosition(gender):
+    """
+    INPUT: Enter M or F for selecting gender
+    OUTPUT: Return the the median age, height and weight of all Gold medal winners for all the events.
+    The events are filter based on predefined list of selected sports.
+    """
+    selected_sports = ('Basketball', 'Boxing', 'Cycling', 'Figure Skating','Gymnastics',
+                   'Judo', 'Rowing','Snowboarding','Speed Skating', 'Swimming',
+                   'Tennis', 'Volleyball',  'Weightlifting', 'Wrestling')
+
+    results = db.session.query(Athletes.event, Athletes.sport, 
+        func.percentile_cont(0.5).within_group(Athletes.age).label('median age'),\
+        func.percentile_cont(0.5).within_group(Athletes.height).label('median height'),\
+        func.percentile_cont(0.5).within_group(Athletes.weight).label('median weight'))\
+        .filter(Athletes.age.isnot(None))\
+        .filter(Athletes.height.isnot(None))\
+        .filter(Athletes.weight.isnot(None))\
+        .filter(Athletes.medal == 'Gold')\
+        .filter(Athletes.sex == gender)\
+        .filter(Athletes.sport.in_(selected_sports))\
+        .group_by(Athletes.event, Athletes.sport)\
+        .all()
+
+    event_body_composition = []
+
+    for event, sport, medianAge, medianHeight, medianWeight in results:
+        body_composition = {}
+        body_composition["event"] = event
+        body_composition["sport"] = sport
+        body_composition["median age"] = medianAge
+        body_composition["median height"] = medianHeight
+        body_composition["median weight"] = medianWeight
+        event_body_composition.append(body_composition)
+
+    return jsonify(event_body_composition)
 
 
 @app.route('/api/test2')
