060c8730a0c33d74e0a064473b0cc3c262fe82ed
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-06-29 20:46:01 -0700

    adding change to zoom in appchart3.js

--- static/js/app_chart3.js
+++ static/js/app_chart3.js
@@ -3,7 +3,7 @@ var tableData = data;
 
 var myMap = L.map("map", {
   center: [0, 0],
-  zoom: 3
+  zoom: 2
  });
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', { 
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
