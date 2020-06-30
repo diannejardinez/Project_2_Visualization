ff7eb2d70bb78325d65bab3ebb2804287f82dcc1
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-06-30 16:31:57 -0700

    Revert "initial commit"
    This reverts commit 97f68a817721207964948ae9ff1db50e588d62cf.

--- config_database.py
+++ config_database.py
@@ -1,4 +1,4 @@
 #######   ENTER THE PASSWORD AND NAME_OF_POSTGRES_DATABASE #######
 
-password = "Seaweed3"
-database_name = "Olympics"
+password = "YOUR PASSCODE HERE"
+database_name = "YOUR DATABASE HERE"
